60386c65443d60e89843fa660e22f19bd2b6ce88
dikakoko <dikakoko04@gmail.com>
2023-10-15 22:27:42 +0700

    Up

---
+++ Formula/get_app.rb
@@ -0,0 +1,11 @@
+class GetApp < Formula
+  desc "render a random joke about Chuck Norris"
+  homepage "https://github.co/dikako/homebrew-app-store-connect"
+  version "1.0.0"
+
+  url "https://github.com/dikako/homebrew-app-store-connect/archive/main.zip", :using => :curl
+
+  def install
+    bin.install "bin/get_app"
+  end
+end
