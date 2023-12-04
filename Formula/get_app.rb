0ee67563ff1dc8e116e97782f2cff72ab439868c
dikakoko <dikakoko04@gmail.com>
2023-12-04 11:52:38 +0700

    Adjust product name by input value

--- Formula/get_app.rb
+++ Formula/get_app.rb
@@ -1,9 +1,9 @@
 class GetApp < Formula
   desc "Get app from app store connect"
   homepage "https://github.com/dikako/homebrew-app-store-connect"
-  version "1.8"
+  version "1.8.1"
 
-  url "https://github.com/dikako/homebrew-app-store-connect/archive/refs/tags/1.8.zip", :using => :curl
+  url "https://github.com/dikako/homebrew-app-store-connect/archive/refs/tags/1.8.1.zip", :using => :curl
 
   depends_on "ruby@3" # Use Ruby 3 as a dependency
 
