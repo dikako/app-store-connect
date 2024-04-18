2f4b0705aab7ce25e4064db2a6890f02cb0b4bf5
dikakoko <dikakoko04@gmail.com>
2024-04-18 12:37:23 +0700

    Add readable info app and improve speed download app

--- Formula/get_app.rb
+++ Formula/get_app.rb
@@ -3,7 +3,7 @@ class GetApp < Formula
   homepage "https://github.com/dikako/homebrew-app-store-connect"
   version "1.8.2"
 
-  url "https://github.com/dikako/homebrew-app-store-connect/archive/refs/tags/1.8.1.zip", :using => :curl
+  url "https://github.com/dikako/homebrew-app-store-connect/archive/refs/tags/1.8.2.zip", :using => :curl
 
   depends_on "ruby@3" # Use Ruby 3 as a dependency
 
